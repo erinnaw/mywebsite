305cec64745c32e5f04ae7f8a3f8e635bb49f12e
erinnaw <83241527+erinnaw@users.noreply.github.com>
2021-08-18 23:18:35 -0700

    initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -9,113 +9,6 @@ const sanitizeHTML = function (str) {
     });
 };
 
-/*---------------linkedin link------------------*/
-$('#linkedin-link').on("mouseover", () => {
-    $('#linkedin-link').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#linkedin-link').removeClass("link-hover");
-});
-
-/*---------------github------------------*/
-$('#github-link').on("mouseover", () => {
-    $('#github-link').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#github-link').removeClass("link-hover");
-});
-
-/*---------------learn blackjack------------------*/
-$('#learnblackjack-link').on("mouseover", () => {
-    $('#learnblackjack-link').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#learnblackjack-link').removeClass("link-hover");
-});
-
-$('#learnblackjack-github').on("mouseover", () => {
-    $('#learnblackjack-github').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#learnblackjack-github').removeClass("link-hover");
-});
-
-/*---------------fp------------------*/
-$('#fp-link').on("mouseover", () => {
-    $('#fp-link').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#fp-link').removeClass("link-hover");
-});
-
-$('#fp-github').on("mouseover", () => {
-    $('#fp-github').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#fp-github').removeClass("link-hover");
-});
-
-/*---------------game session maker------------------*/
-$('#gamesessionmaker-link').on("mouseover", () => {
-    $('#gamesessionmaker-link').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#gamesessionmaker-link').removeClass("link-hover");
-});
-
-$('#gamesessionmaker-github').on("mouseover", () => {
-    $('#gamesessionmaker-github').addClass("link-hover");
-})
-.on("mouseout", () => {
-    $('#gamesessionmaker-github').removeClass("link-hover");
-});
-
-/*-------------------arrows-------------------------*/
-$('#learnblackjack-arrow').on("mouseover", () => {
-    $('#learnblackjack-arrow').addClass('arrow-bounceright');
-})
-.on("mouseout", () => {
-    $('#learnblackjack-arrow').removeClass('arrow-bounceright');
-})
-.on("click", () => {
-    if (window.open('https://learnblackjack.erinnawidjaja.com', '_blank')) {
-        console.log("Learn BlackJack");
-    }
-    else {
-        alert("Your popup blocker has prevented the website from loading.");
-    }
-});
-
-$('#fp-arrow').on("mouseover", () => {
-    $('#fp-arrow').addClass('arrow-bounceright');
-})
-.on("mouseout", () => {
-    $('#fp-arrow').removeClass('arrow-bounceright');
-})
-.on("click", () => {
-    if (window.open('https://fp.erinnawidjaja.com', '_blank')) {
-        console.log("FingerprintJS demo");
-    }
-    else {
-        alert("Your popup blocker has prevented the website from loading.");
-    }
-});
-
-$('#gamesessionmaker-arrow').on("mouseover", () => {
-    $('#gamesessionmaker-arrow').addClass('arrow-bounceright');
-})
-.on("mouseout", () => {
-    $('#gamesessionmaker-arrow').removeClass('arrow-bounceright');
-})
-.on("click", () => {
-    if (window.open('http://34.220.94.122/', '_blank')) {
-        console.log("Game Session Maker");
-    }
-    else {
-        alert("Your popup blocker has prevented the website from loading.");
-    }
-});
-
 /*-------------------icon links---------------------*/
 $('#linkedin').on("mouseover", () => {
     $('#linkedin').addClass('icon-img-animation');
@@ -125,7 +18,6 @@ $('#linkedin').on("mouseover", () => {
 })
 .on("click", () => {
     if (window.open('https://www.linkedin.com/in/erinna-widjaja/', '_blank')) {
-        console.log("linkedin");
     }
     else {
         alert("Your popup blocker has prevented the website from loading.");
@@ -140,7 +32,20 @@ $('#github').on("mouseover", () => {
 })
 .on("click", () => {
     if (window.open('https://github.com/erinnaw', '_blank')) {
-        console.log("Github");
+    }
+    else {
+        alert("Your popup blocker has prevented the website from loading.");
+    }    
+});
+
+$('#email').on("mouseover", () => {
+    $('#email').addClass('icon-img-animation');
+})
+.on("mouseout", () => {
+    $('#email').removeClass('icon-img-animation');
+})
+.on("click", () => {
+    if (window.open('', '_blank')) {
     }
     else {
         alert("Your popup blocker has prevented the website from loading.");
